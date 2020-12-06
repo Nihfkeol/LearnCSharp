a01a79b93504a19af451947b95f1cb3272447565
Nihfkeol <nihfkeol@163.com>
2020-12-06 17:00:26 +0800

    进销存管理系统

---
+++ GoodsStruct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/**
+ * 进销存管理系统
+ */
+namespace GoodsStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("库存盘点信息如下：");
+            cStockInfo csi1 = new cStockInfo();
+            csi1.FullName = "空调";
+            csi1.TradeTpye = "TYPE-1";
+            csi1.Qty = 2000;
+            csi1.ShowInfo();
+            cStockInfo csi2 = new cStockInfo();
+            csi2.FullName = "空调";
+            csi2.TradeTpye = "TYPE-2";
+            csi2.Qty = 3500;
+            csi2.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GoodsStruct/cStockInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * 商品类
+ */ 
+namespace GoodsStruct
+{
+    class cStockInfo
+    {
+        private string tradeCode;
+        private string fullName;
+        private string tradeTpye;
+        private string standard;
+        private string standeunit;
+        private string produce;
+        private float qty = 0;
+        private float price = 0;
+        private float averagePrice = 0;
+        private float salePrice = 0;
+        private float check = 0;
+        private float upperLimit = 0;
+        private float lowerLimit = 0;
+
+        public string TradeCode { get => tradeCode; set => tradeCode = value; }
+        public string FullName { get => fullName; set => fullName = value; }
+        public string TradeTpye { get => tradeTpye; set => tradeTpye = value; }
+        public string Standard { get => standard; set => standard = value; }
+        public string Standeunit { get => standeunit; set => standeunit = value; }
+        public string Produce { get => produce; set => produce = value; }
+        public float Qty { get => qty; set => qty = value; }
+        public float Price { get => price; set => price = value; }
+        public float AveragePrice { get => averagePrice; set => averagePrice = value; }
+        public float SalePrice { get => salePrice; set => salePrice = value; }
+        public float Check { get => check; set => check = value; }
+        public float UpperLimit { get => upperLimit; set => upperLimit = value; }
+        public float LowerLimit { get => lowerLimit; set => lowerLimit = value; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("仓库中存有{0}型号{1}{2}台", TradeTpye, FullName, Qty);
+        }
+    }
+}
